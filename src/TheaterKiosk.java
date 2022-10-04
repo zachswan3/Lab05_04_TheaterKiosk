2bebdeeeab3eebab50c1140647b2aec4ec90dce6
zachswan9 <swanzn@mail.uc.edu>
2022-10-04 16:29:16 -0400

    Completed task 4

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,19 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        //As people pass through an entry kiosk at the theater, they are prompted to enter their age.  If they are 21
+        // or older, they get a paper wrist band.  Code a logic program that asks the user to enter their age and then
+        // if they are 21 or over displays a  message that they get a wrist band.  (Note that the program does nothing
+        // if they are not 21 or over…)
+        //
+        //Test runs:   (insert the output widow copies here for the 2 test runs)
+        //If you were directed to use GitHub, be sure to create the repo and commit and push it.
+
+        int age;
+
+        age = 22;
+
+        if (age >= 21){
+            System.out.println("You get a wristband!");
+        }
+    }
+}
